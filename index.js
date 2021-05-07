342a134e437775757a914da12510e60ffc9f2219
Rosa <rosarenee@att.net>
2021-05-07 04:52:50 -0500

    Completed assignment

---
+++ index.js
@@ -0,0 +1,29 @@
+function add(r1,r2){
+
+    return r1+r2;
+}
+function subtract(s1,s2){
+return s1-s2
+}
+function multiply(m1,m2){
+    return m1*m2
+}
+function divide(d1,d2){
+    return d1/d2
+}
+
+function increment(a){
+    a++
+    return a;
+}
+function decrement(a){
+    a--
+    return a;
+}
+ function makeInt(n, base ='10'){
+    return parseInt(n, base)
+    
+ }
+function preserveDecimal(n){
+    return parseFloat(n)
+}
